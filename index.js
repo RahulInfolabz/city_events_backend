349a5a41288f0b5196acc3d195c19075a5397f7b
rahulinfolabz <rahulinfolabz@gmail.com>
2026-05-27 14:18:13 +0530

    Index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:5174", "https://cityeventfrontendproject.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
